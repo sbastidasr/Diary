018944136dd4d4bc333fca02d45e6afcd08a544f
Sebastian Bastidas <sbastidasr@Sebastians-MacBook-Pro.local>
2014-07-10 11:29:36 -0500

    Moved Core Data to its own class

---
+++ Diary/AQCoreDataStack.h
@@ -0,0 +1,20 @@
+//
+//  AQCoreDataStack.h
+//  Diary
+//
+//  Created by Sebastian Bastidas on 7/10/14.
+//  Copyright (c) 2014 Adtq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AQCoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
